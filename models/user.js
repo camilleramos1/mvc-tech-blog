7e2011c78215b86d6ae10829533f74b7054f4840
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-07-27 12:26:34 -0600

    heroku predeploy

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
+    checkPassword(checkPassword) {
+        return bcrypt.compareSync(checkPassword, this.password);
     }
 }
 
